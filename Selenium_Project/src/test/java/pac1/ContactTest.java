1b2598ea67862fb7e23ba99f00711ea4df68560f
RAKESH CHOUDHARI <RAKESH CHOUDHARI@DESKTOP-MEAOL4U.mshome.net>
2024-07-30 15:44:12 +0530

    first push

---
+++ Selenium_Project/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,21 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+@Test
+public void createcontacttest() {
+	String URL=System.getProperty("url");
+	String BROWSER=System.getProperty("browser");
+	String USERNAME=System.getProperty("username");
+	String PASSWORD=System.getProperty("password");
+	System.out.println(URL);
+	System.out.println(BROWSER);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
+	System.out.println("execute createcontact");
+}
+@Test
+public void modifycontatatest() {
+	System.out.println("execute modifycontact");
+}}
---
+++ Selenium_Project/src/test/java/pac2/Org.java
@@ -0,0 +1,14 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class Org {
+	@Test
+	public void createorgtest() {
+		System.out.println("execute createorgTest");
+	}
+	@Test
+	public void modifyorgtest() {
+		System.out.println("execute modifyorgTest");
+	}
+}
